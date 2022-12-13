bbc18e3a49aa689d1ea2e5f425e6a50c5d8eb5d2
David Currie <davidalexandercurrie@gmail.com>
2022-12-12 23:49:41 -0500

    fixed

--- javascript/accelerometer/script.js
+++ javascript/accelerometer/script.js
@@ -1,8 +1,8 @@
 const acl = new Accelerometer({ frequency: 60 });
 acl.addEventListener('reading', () => {
-  document.getElementById('x').innerText = `x: ${acl.x}`;
-  document.getElementById('y').innerText = `y: ${acl.y}`;
-  document.getElementById('z').innerText = `z: ${acl.z}`;
+  document.getElementById('x').innerText = `x: ${acl.x.toFixed(5)}`;
+  document.getElementById('y').innerText = `y: ${acl.y.toFixed(5)}`;
+  document.getElementById('z').innerText = `z: ${acl.z.toFixed(5)}`;
 });
 
 acl.start();
